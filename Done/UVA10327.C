b0f088769cb4e1bbb50ec6420b2368052a1af1f0
Tyler Davidson <tdavidso@ualberta.ca>
2014-01-26 21:46:43 -0700

    update

--- Done/UVA10327.C
+++ Done/UVA10327.C
@@ -26,12 +26,10 @@ int findFlipsfor(const int N)
 		nums.push_back(newNum);
 	}
 
-	for (int j = 1; j < N; ++j){
-		for (int i = j-1; i >= 0; --i){
+	for (int j = 1; j < N; ++j)
+		for (int i = j-1; i >= 0; --i)
 			if (nums[i] > nums[j])
 				++flips;
-		}
-	}
 
 	return flips;
 }
