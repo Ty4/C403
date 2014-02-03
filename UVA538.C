46d7fdd7b8293a530cce6fdf89b70b11f65db22f
Tyler Davidson <tdavidso@ualberta.ca>
2014-02-03 11:01:38 -0700

    new UVA probs

---
+++ UVA538.C
@@ -0,0 +1,21 @@
+/* by Tyler Davidson
+ *
+ * Balancing Bank Accounts: 	NONTRIVIAL		(Arithmetic)
+ *
+*/
+
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+int n, t;
+
+int main()
+{
+	scanf("%d %d", &n, &t);
+	while (n != 0 && t != 0){
+
+		scanf("%d %d", &n, &t);
+	}
+}
