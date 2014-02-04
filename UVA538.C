c081062f43f4715627c66107c091d41ee062765e
Ty4 <tdavidso@ualberta.ca>
2014-02-03 21:04:48 -0700

    finishedish 538

--- UVA538.C
+++ UVA538.C
@@ -9,15 +9,21 @@
 #include <map>
 #include <vector>
 
+#define psi pair<string,int>
+#define msi map<string,int>
+#define msp map<string,psi>
+
 using namespace std;
 
 class UnionFind
 {
 private:
 	// debts maps the debtee to the pair of owee and amount
-	map<string, pair<string,int> > debts;
-	map<string, int> rank;
+	msp debts;
+	msi rank;
 public:
+	msp getDebts(){ return debts; }
+
 	UnionFind(vector<string> & people)
 	{
 		for (vector<string>::iterator it = people.begin();
@@ -35,16 +41,23 @@ public:
 			return findDebtor(debts[debtee].first);
 	}
 
-	l
 	void update(const string & debtee, const string & debtor, const int amount)
 	{
 		if (debts[debtor].first == debtor){
 			debts[debtee].first = debtor;
 			debts[debtee].second = amount;
 		}
+		else if (debts[debtor].first == debtee){
+			debts[debtor].second -= amount;
+		}
 		else {
 			debts[debtor].second -= amount;
-			update(debtee, debts[debtor].first, amount);
+			if (debts[debtor].first == debts[debtee].first){
+				debts[debtee].second += amount;
+				update(debtee, debts[debtor].first, debts[debtee].second);
+			}
+			else
+				update(debtee, debts[debtor].first, amount);
 		}
 	}
 };
@@ -52,6 +65,7 @@ int n, t;
 
 int main()
 {
+	int cases = 0;
 	scanf("%d %d", &n, &t);
 	while (n != 0 && t != 0){
 		// read in the names
@@ -61,9 +75,25 @@ int main()
 			cin >> name;
 			tmp.push_back(name);
 		}
-
+		UnionFind debts(tmp);
 		// read in the transactions
-		// process transactions
+		for (int i = 0; i < t; ++i){
+			string p1, p2;
+			int amnt;
+			cin >> p1 >> p2 >> amnt;
+			debts.update(p2,p1,amnt);
+		}
+		printf("Case #%d\n", ++cases);
+		msp debtMap = debts.getDebts();
+		for (msp::iterator it = debtMap.begin(); it != debtMap.end(); ++it){
+			if ((it->first) != (it->second).first){
+				printf("%s %s %d\n", 
+					(it->first).c_str(), 
+					((it->second).first).c_str(),
+					(it->second).second);
+			}
+		}
+		printf("\n");
 		scanf("%d %d", &n, &t);
 	} /* end while */
 } /* end main */
